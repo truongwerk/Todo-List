ca17f04ebd71a1b656821ea569a993cd188e34da
Tong Quang Truong <truongwerk@gmail.com>
2022-01-05 23:29:56 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Display and hide Task Form
 const taskForm = document.getElementById("taskForm");
 const addTaskButton = document.getElementById("addTaskButton");
 addTaskButton.addEventListener("click", function () {
@@ -8,3 +9,30 @@ window.addEventListener("click", function (e) {
 		taskForm.style.display = "none";
 	}
 });
+
+let myTodo = [];
+
+// Add Task
+function todo(task, targetDate, description, project) {
+	this.task = task;
+	this.targetDate = targetDate;
+	this.description = description;
+	this.project = project;
+}
+
+const newTaskForm = document.getElementById("newTaskForm");
+newTaskForm.addEventListener("submit", submitNewTodo);
+function submitNewTodo() {
+	const task = document.getElementById("inputTask").value;
+	const targetDate = document.getElementById("inputDate").value;
+	const description = document.getElementById("inputDescription").value;
+	const project = document.getElementById("addToProject").value;
+	const newTodo = new todo(task, targetDate, description, project);
+	myTodo.push(newTodo);
+	taskForm.style.display = "none";
+	document.getElementById("inputTask").value = null;
+	document.getElementById("inputDate").value = null;
+	document.getElementById("inputDescription").value = null;
+	document.getElementById("addToProject").value = null;
+	console.table(myTodo);
+}
