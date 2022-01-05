1f7602be2b838003d9f9b1938f8d703433acfe86
Tong Quang Truong <truongwerk@gmail.com>
2022-01-05 21:19:43 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello2");
+const taskForm = document.getElementById("taskForm");
+const addTaskButton = document.getElementById("addTaskButton");
+addTaskButton.addEventListener("click", function () {
+	taskForm.style.display = "flex";
+});
+window.addEventListener("click", function (e) {
+	if (e.target == taskForm) {
+		taskForm.style.display = "none";
+	}
+});
