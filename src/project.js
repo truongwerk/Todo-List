39c255af6918de46ae9c0cd995cad84f3388de89
Tong Quang Truong <truongwerk@gmail.com>
2022-01-11 10:50:17 +0700

    m

---
+++ src/project.js
@@ -0,0 +1,10 @@
+import { getTodo, setTodo, getProject, setProject, getMode } from ".";
+
+function projectFunction() {
+	let moduleTodo = getTodo();
+	let moduleProject = getProject();
+}
+
+function displayProject(moduleProject){
+	
+}
